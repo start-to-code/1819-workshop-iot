97bf222c1c07124869875bbc7f4eab45f3c5abf0
drdynscript <drdynscript@gmail.com>
2019-01-16 21:25:25 +0100

    Add comments to main.js in Ambilight client

--- ambilight/app/assets/js/main.js
+++ ambilight/app/assets/js/main.js
@@ -1,3 +1,26 @@
+/*
+Start 2 Code
+============
+Workshop: Internet of Things
+Application: Ambilight
+Description: 
+  - Change the RGBMatrix on the RaspberryPi via Firestore
+  - Input-fields: color and switch
+  - Listen to updates in the corresponding Firestore document (and update the UI)
+Technology:
+  - HTML
+  - CSS
+    - Libraries:
+      - fontawesome
+  - JavaScript
+    - Libraries:
+      - firebasejs
+Cloud:
+  - Firestore (Firebase)
+*/
+
+'use strict'
+
 const config = {
   firebase: {
     apiKey: 'AIzaSyAb_oy301KscmyVEypq_3zvmZT-jABqG6I',
